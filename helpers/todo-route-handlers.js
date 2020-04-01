ee420f3f54da7125abaaf493ffb1bcb35f18405d
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-01 16:39:47 -0500

    Refactor to use helpers for route-handlers.

---
+++ helpers/todo-route-handlers.js
@@ -0,0 +1,33 @@
+const Todo = require('../models/todo');
+
+exports.getTodos = (req, res, next) => {
+  Todo.find()
+    .then(todos => res.json(todos))
+    .catch(console.error);
+};
+
+exports.postTodo = (req, res, next) => {
+  Todo.create(req.body)
+    .then(todo => res.status(201).json(todo))
+    .catch(console.error);
+};
+
+exports.getTodo = (req, res, next) => {
+  Todo.findById(req.params.id)
+    .then(todo => res.json(todo))
+    .catch(console.error);
+};
+
+exports.putTodo = (req, res, next) => {
+  Todo.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+    .then(todo => res.json(todo))
+    .catch(console.error);
+};
+
+exports.deleteTodo = (req, res, next) => {
+  Todo.findOneAndRemove(req.params.id)
+    .then(todo => res.json(todo))
+    .catch(console.error);
+};
+
+module.exports = exports;
--- routes/todos.js
+++ routes/todos.js
@@ -1,38 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const Todo = require('../models/todo');
+const { getTodos, postTodo, getTodo, putTodo, deleteTodo } = require('../helpers/todo-route-handlers');
 
-const router = express.Router();
 const jsonBodyParser = bodyParser.json();
+const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  Todo.find()
-    .then(todos => res.json(todos))
-    .catch(console.error);
-});
+router.route('/')
+  .get(getTodos)
+  .post(jsonBodyParser, postTodo);
 
-router.post('/', jsonBodyParser, (req, res, next) => {
-  Todo.create(req.body)
-    .then(todo => res.status(201).json(todo))
-    .catch(console.error);
-});
-
-router.get('/:id', (req, res, next) => {
-  Todo.findById(req.params.id)
-    .then(todo => res.json(todo))
-    .catch(console.error);
-});
-
-router.put('/:id', jsonBodyParser, (req, res, next) => {
-  Todo.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
-    .then(todo => res.json(todo))
-    .catch(console.error);
-});
-
-router.delete('/:id', (req, res, next) => {
-  Todo.findOneAndRemove(req.params.id)
-    .then(todo => res.json(todo))
-    .catch(console.error);
-});
+router.route('/:id')
+  .get(getTodo)
+  .put(jsonBodyParser, putTodo)
+  .delete(deleteTodo);
 
 module.exports = router;
