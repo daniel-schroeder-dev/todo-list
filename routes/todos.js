1d01ee1ab238d17d17bf51ff2cbd673df76a3559
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-03-31 21:53:35 -0500

    Add body-parser.

--- routes/todos.js
+++ routes/todos.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Todo = require('../models/todo');
+
 const router = express.Router();
+const jsonBodyParser = bodyParser.json();
 
 router.get('/', (req, res, next) => {
   Todo.find()
     .then(todos => res.json(todos))
     .catch(console.error);
+});
+
+router.post('/', jsonBodyParser, (req, res, next) => {
+  console.log(req.body);
+  res.sendStatus(200);
 });
 
 module.exports = router;
