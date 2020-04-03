12cfb4fdeb4d446a505b18f81f6de9bfbc1671d6
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-02 20:34:26 -0500

    Add newly created task to the DOM.

--- public/main.js
+++ public/main.js
@@ -1,17 +1,47 @@
 const form = document.querySelector('.add-task__form');
 const addTaskInput = document.querySelector('.add-task__input');
+const taskList = document.querySelector('.task-list');
 
 form.addEventListener('submit', async e => {
+  
   e.preventDefault();
-  console.log(addTaskInput.value);
+
+  const taskName = addTaskInput.value;
+  addTaskInput.value = '';
+
   const response = await fetch('/api/todos', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({ name: addTaskInput.value }),
+    body: JSON.stringify({ name: taskName }),
   });
+  
   const task = await response.json();
-  console.log(task);
+
+  taskList.append(createTaskDOMElement(taskName));
+
 });
+
+function createTaskDOMElement(taskName) {
+  
+  const li = document.createElement('li');
+  const taskLink = document.createElement('a');
+  const removeIcon = document.createElement('a');
+  
+  li.classList.add('task-list__task');
+  taskLink.classList.add('task-list__task__link');
+  removeIcon.classList.add('task-list__task__remove-icon');
+
+  taskLink.href = '/';
+  removeIcon.setAttribute('aria-label', 'remove-task');
+
+  taskLink.textContent = taskName;
+  removeIcon.innerHTML = '&times;';
+
+  li.append(taskLink);
+  li.append(removeIcon);
+
+  return li;
+}
 
